7ddd58c612a57da8bedf888d61f449bae4213852
CyrelDigang <digangc@gmail.com>
2025-09-08 19:47:56 +0800

    merge master

---
+++ src/components/About/Toolstack.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import {
+  SiVisualstudiocode,
+  SiPostman,
+  SiWindows,
+  SiIntellijidea,
+  SiApachenetbeanside,
+  SiGooglecolab,
+} from "react-icons/si";
+
+function Toolstack() {
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiVisualstudiocode />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiGooglecolab />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiApachenetbeanside />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiIntellijidea />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiPostman />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiWindows />
+      </Col>
+    </Row>
+  );
+}
+
+export default Toolstack;
